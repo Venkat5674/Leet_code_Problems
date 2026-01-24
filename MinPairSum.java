d63031d584a6af0a91a75ce270a72458e27afa8d
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2026-01-24 23:15:02 +0530

    Create MinPairSum.java

---
+++ MinPairSum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        int max = 0;
+
+        for(int i = 0; i < n/2; i++){
+            int pairsum = nums[i] + nums[n-1-i];
+            max = Math.max(max, pairsum);
+        }
+        return max;
+    }
+}
