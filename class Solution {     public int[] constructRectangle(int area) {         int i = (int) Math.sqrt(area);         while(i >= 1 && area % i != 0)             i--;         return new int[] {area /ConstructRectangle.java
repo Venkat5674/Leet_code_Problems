792f6355882cffae10f6df29999d99a4d397b167
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2026-02-04 23:15:08 +0530

    Delete class Solution {     public int[] constructRectangle(int area) {         int i = (int) Math.sqrt(area);         while(i >= 1 && area % i != 0)             i--;         return new int[] {area  directory

--- class Solution {     public int[] constructRectangle(int area) {         int i = (int) Math.sqrt(area);         while(i >= 1 && area % i != 0)             i--;         return new int[] {area /ConstructRectangle.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int[] constructRectangle(int area) {
-        int i = (int) Math.sqrt(area);
-        while(i >= 1 && area % i != 0)
-            i--;
-        return new int[] {area / i, i};
-    }
-}
