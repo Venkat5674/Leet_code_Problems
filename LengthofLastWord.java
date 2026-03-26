0d01d83883450f37b1da3c54f51d1e057dfdcfc3
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2026-03-26 19:44:30 +0530

    Improve comments in lengthOfLastWord method
    Updated comments for clarity in lengthOfLastWord method

--- LengthofLastWord.java
+++ LengthofLastWord.java
@@ -3,17 +3,17 @@ class Solution {
        int length = 0;
         int i = s.length() - 1;
 
-        // Step 1: Skip trailing spaces
+        // Step 1: Skip trailing spaces in string
         while (i >= 0 && s.charAt(i) == ' ') {
             i--;
         }
 
-        // Step 2: Count last word
+        // Step 2: Count last word of String
         while (i >= 0 && s.charAt(i) != ' ') {
             length++;
             i--;
         }
-
+        // return the length of Long Word
         return length; 
     }
 }
