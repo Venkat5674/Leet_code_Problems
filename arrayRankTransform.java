15f3d8fcb817f4e1228ed0485f414c2451957c40
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2026-07-12 22:17:29 +0530

    Create arrayRankTransform.java

---
+++ arrayRankTransform.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
+        int m = 0;
+
+        for (int i = 0; i < sorted.length; i++) {
+            int x = sorted[i];
+
+            if (m == 0 || sorted[m - 1] != x) {
+                sorted[m++] = x;
+            }
+        }
+
+        int[] unique = Arrays.copyOf(sorted, m);
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Arrays.binarySearch(unique, arr[i]) + 1;
+        }
+
+        return arr;
+    }
+}
